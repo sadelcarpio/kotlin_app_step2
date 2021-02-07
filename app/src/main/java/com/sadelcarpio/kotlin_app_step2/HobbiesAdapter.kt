a15563fad93a2f444541359416a26f83710ad14e
sadelcarpio <sadelcarpioa@gmail.com>
2021-02-07 14:59:00 -0500

    Agregado RecyclerView (aunque no entendí mucho xd)

--- app/src/main/java/com/sadelcarpio/kotlin_app_step2/HobbiesAdapter.kt
+++ app/src/main/java/com/sadelcarpio/kotlin_app_step2/HobbiesAdapter.kt
@@ -1,16 +1,42 @@
 package com.sadelcarpio.kotlin_app_step2
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item.view.*
 
 class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>() {
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun setData(hobby: Hobby?, position: Int) {
+
+        var currentHobby: Hobby? = null
+        var currentPosition: Int = 0
+
+        init {
+            itemView.setOnClickListener {
+                Toast.makeText(context, "Clickeaste " + currentHobby!!.title, Toast.LENGTH_SHORT).show()
+            }
+
+            itemView.imgShare.setOnClickListener {
+                val message: String = "Mi hobby es: " + currentHobby!!.title //convierte el texto ingresado a string
+                val intent = Intent()  //no tiene argumentos porque no sabemos a dónde nos llevará
+                intent.action = Intent.ACTION_SEND //propiedad de enviar
+                intent.putExtra(Intent.EXTRA_TEXT, message) //el key predefinido es Intent.EXTRA_TEXT (reconocible por otras apps)
+                intent.type = "text/plain" //tipo de dato a compartir
+
+                context.startActivity(Intent.createChooser(intent, "Compartir en :")) //muestra la ventana de compartir
+            }
+        }
+
+        fun setData(hobby: Hobby?, pos: Int) {
             itemView.txvTitle.text = hobby!!.title
+
+            this.currentHobby = hobby
+            this.currentPosition = pos
         }
     }
 
